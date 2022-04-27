25237af48549aba0fe820e6a6d730958f1464f63
Филимонов Павел <99137277+painerit@users.noreply.github.com>
2022-04-28 00:49:01 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -6,10 +6,10 @@ template<typename T>
 class TPQueue {
  private :
   struct ITEM {
-    T data;
+    T content;
     ITEM * next;
     ITEM* prev;
-};
+  };
   TPQueue :: ITEM * create(const T &);
   ITEM* head;
   ITEM* tail;
@@ -24,44 +24,44 @@ struct SYM {
   int prior;
 };
 template<typename T>
-typename TPQueue<T>::ITEM* TPQueue<T>::create(const T& data) {
+typename TPQueue<T>::ITEM* TPQueue<T>::create(const T& content) {
   ITEM * item = new ITEM;
-  item -> data = data;
+  item -> content = content;
   item -> next = nullptr;
   item -> prev = nullptr;
   return item;
 }
 template < typename T >
-void TPQueue <T>::push(const T& data) {
-  ITEM * temp = head;
-  ITEM * item = create(data);
-  while (temp && temp -> data.prior >= data.prior )
-    temp = temp -> next ;
-  if (!temp && head) {
+void TPQueue <T>::push(const T& content) {
+  ITEM * tmp = head;
+  ITEM * item = create(content);
+  while (tmp && tmp -> content.prior >= content.prior )
+    tmp = tmp -> next;
+  if (!tmp && head) {
     tail -> next = item;
     tail -> next -> prev = tail;
     tail = item;
-  } else if (!temp && !head) {
+  } else if (!tmp && !head) {
       head = tail = item;
-  } else if (!temp -> prev) {
-    temp -> prev = item;
-    item -> next = temp;
+  } else if (!tmp -> prev) {
+    tmp -> prev = item;
+    item -> next = tmp;
     head = item;
   } else {
-      temp -> prev -> next = item;
-      item -> prev = temp -> prev;
-      item -> next = temp;
-      temp -> prev = item;
+      tmp -> prev -> next = item;
+      item -> prev = tmp -> prev;
+      item -> next = tmp;
+      tmp -> prev = item;
   }
 }
-template < typename T >
+tmplate < typename T >
 T TPQueue<T>::pop() {
-  ITEM * temp = head -> next;
-  if (temp)
-    temp -> prev = nullptr;
-  T data = head -> data;
+  ITEM * tmp = head -> next;
+  if (tmp)
+    tmp -> prev = nullptr;
+  T content = head -> content;
   delete head;
-  head = temp;
-  return data;
+  head = tmp;
+  return content;
 }
 #endif  // INCLUDE_TPQUEUE_H_
